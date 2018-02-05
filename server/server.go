740a5550ffb059f0bfa4a86d37697622bd00977c
Ranjith Kumar Shetty <ranjithk@oath.com>
2018-02-05 15:00:23 -0800

    Enable tls mode

--- server/server.go
+++ server/server.go
@@ -9,17 +9,21 @@ import (
 
 	pb "github.com/ranjithkumarshetty/sample-grpc-serverstream/protos"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
-	port = flag.Int("port", 4443, "The server port")
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile = flag.String("cert_file", "", "The TLS cert file")
+	keyFile  = flag.String("key_file", "", "The TLS key file")
+	port     = flag.Int("port", 4443, "The server port")
 )
 
 type streamerServer struct {
 }
 
 func (s *streamerServer) StreamMessages(msg *pb.Message, srv pb.Streamer_StreamMessagesServer) error {
-	log.Printf("Received message from client:%v\n", msg)
+	log.Printf("Received message from client:\"%s\"", msg)
 	i := 1
 	for 1 == 1 {
 		if err := srv.Send(&pb.Message{fmt.Sprintf("Server hello reply times:%v", i)}); err != nil {
@@ -37,7 +41,21 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	grpcServer := grpc.NewServer()
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = "certificate.pem"
+		}
+		if *keyFile == "" {
+			*keyFile = "server_key.pem"
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+	grpcServer := grpc.NewServer(opts...)
 	srv := &streamerServer{}
 	pb.RegisterStreamerServer(grpcServer, srv)
 	grpcServer.Serve(lis)
